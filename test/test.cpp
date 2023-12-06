374bb38071e95464e2cf752fc7093b136a149165
IskXCr <iskxcr@outlook.com>
2023-12-06 12:56:00 +0800

    Update test

--- test/test.cpp
+++ test/test.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/dnn/all_layers.hpp>
 #include <iostream>
 #include <chrono>
+#include <random>
 using namespace std;
 using namespace cv;
 
@@ -20,7 +21,7 @@ void printNetInfo(dnn::Net net) {
     }
 }
 
-void printMatRec(const cv::Mat& mat, int depth, int *mat_dims, int *dims) {
+void printMatRec(const cv::Mat& mat, int depth, int* mat_dims, int* dims) {
     for (int i = 0; i < mat.dims - depth; i++) {
         cout << " ";
     }
@@ -58,20 +59,21 @@ void printMat(const cv::Mat& mat) {
 }
 
 
-void cal(Mat &input1, Mat &input2, Mat &output, dnn::Net& net) {
+void cal(Mat& input1, Mat& input2, Mat& output, dnn::Net& net) {
     net.setInput(input1, "input1");
     net.setInput(input2, "input2");
 
+    cout << "\033[93m" << "First forwarding." << "\033[0m\n";
     output = net.forward();
+    cout << "\033[93m" << "Formal forwarding." << "\033[0m\n";
 
     auto begin = std::chrono::high_resolution_clock::now();
-    CV_LOG_DEBUG(NULL, "start forwarding");
     output = net.forward();
     auto end = std::chrono::high_resolution_clock::now();
     cout << "\033[93mTime elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " ms\033[0m\n";
 }
 
-Mat testSingle(Mat &input1, Mat &input2, dnn::Net& net, bool useVK, bool print = false, bool useInternelPrint = true)
+Mat testSingle(Mat& input1, Mat& input2, dnn::Net& net, bool useVK, bool print = false, bool useInternelPrint = true)
 {
     Mat output;
     if (useVK)
@@ -112,13 +114,14 @@ Mat testSingle(Mat &input1, Mat &input2, dnn::Net& net, bool useVK, bool print =
 
 void verifyResult(Mat& mat1, Mat& mat2)
 {
-    for (auto it1 = mat1.begin<float>(), it2 = mat2.begin<float>(); it1 != mat1.end<float>(); ++it1, ++it2)
+    size_t sz = 0;
+    for (auto it1 = mat1.begin<float>(), it2 = mat2.begin<float>(); it1 != mat1.end<float>(); ++it1, ++it2, ++sz)
     {
         if (std::fabs(*it1 - *it2) > 1e-9)
         {
-            cout << "\033[91mElement unmatch: " << *it1 << " != " << *it2 << "\033[0m\n";
-            //exit(1);
-            return;
+            cout << "\033[91mElement unmatch: " << *it1 << " != " << *it2 << ", at " << sz << "\033[0m\n";
+            abort();
+            //return;
         }
     }
     cout << "\033[92mResults passed verification.\033[0m\n";
@@ -138,23 +141,29 @@ void validityTest(dnn::Net& net)
     verifyResult(output1, output2);
 }
 
-void speedTest(dnn::Net &net)
+void speedTest(dnn::Net& net)
 {
     Mat input1, input2;
 
     int matDimH = 4096, matDimW = 4096;
     input1 = Mat::ones(matDimH, matDimW, CV_32F);
     input2 = Mat::ones(matDimH, matDimW, CV_32F);
-    input1.at<float>(2000, 723) = 608.53f;
-    input1.at<float>(712, 4) = 123.1231f;
-    input1.at<float>(38, 218) = 21.12f;
-    input1.at<float>(64, 213) = 2813.2f;
-    input1.at<float>(2000, 0) = 213.231f;
-    input2.at<float>(64, 723) = -27.0f;
-    input2.at<float>(128, 4) = 18.5f;
-    input2.at<float>(256, 218) = -212223.9f;
-    input2.at<float>(2000, 0) = -1.7e4f;
-    input2.at<float>(274, 0) = 2.3e5f;
+    mt19937 rng;
+    uniform_real_distribution<float> dist3;
+    uniform_int_distribution<int> distidx(0, 4096 - 1);
+    uniform_int_distribution<int> distsel(0, 1);
+
+    for (int i = 0; i < 16384; ++i)
+    {
+        if (distsel(rng) == 0)
+        {
+            input1.at<float>(distidx(rng), distidx(rng)) = dist3(rng) * 1e5;
+        }
+        else
+        {
+            input2.at<float>(distidx(rng), distidx(rng)) = dist3(rng) * 1e5;
+        }
+    }
 
     Mat output1 = testSingle(input1, input2, net, true, false);
     Mat output2 = testSingle(input1, input2, net, false, false);
@@ -171,7 +180,7 @@ int main() {
     params.type = "NaryEltwise";
     params.set("operation", "add");
     net.addLayer(params.name, params.type, params);
-    net.setInputsNames({"input1", "input2"});
+    net.setInputsNames({ "input1", "input2" });
     net.connect(0, 0, 1, 0);
     net.connect(0, 1, 1, 1);
 
